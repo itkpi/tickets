775a4b6d1bc2304f9d68f371b4ead66d9358d299
Roman Rader <roman.rader@gmail.com>
2016-04-30 10:15:17 +0300

    callback

--- tickets/campaigns/api_views.py
+++ tickets/campaigns/api_views.py
@@ -1,4 +1,11 @@
+import logging
+
+from campaigns.models import Cart
+from liqpay.liqpay import LiqPay
 from restless.views import Endpoint
+from tickets.settings import LIQPAY_PUBLIC, LIQPAY_PRIVATE
+
+logger = logging.getLogger(__name__)
 
 
 class HelloWorld(Endpoint):
@@ -12,10 +19,17 @@ class HelloWorld(Endpoint):
 
 
 class LiqPayS2S(Endpoint):
-    def get(self, request):
-        name = request.params.get('name', 'World')
-        return {'message': 'Hello, %s!' % name}
+    def post(self, request, cart_uid):
+        logger.info("API Call")
+        cart = Cart.objects.get(uid=cart_uid)
+        logger.info("cart {}".format(cart.uid))
+        logger.info("POST {}".format(str(request.POST)))
+        data = request.POST['data']
+        signature = request.POST['signature']
+        logger.info("data: {}, signature: {}".format(data, signature))
 
-    def post(self, request):
-        name = request.params.get('name', 'World')
-        return {'message': 'POST Hello, %s!' % name}
+        liqpay = LiqPay(LIQPAY_PUBLIC, LIQPAY_PRIVATE)
+        sign = liqpay.str_to_sign(LIQPAY_PRIVATE.encode() + data.encode() + LIQPAY_PRIVATE.encode())
+        logger.info("calculated signature {}".format(sign))
+
+        return {'status': 'OK'}
--- tickets/tickets/local_settings.py
+++ tickets/tickets/local_settings.py
@@ -1,1 +1,24 @@
 DEBUG = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'campaigns': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
