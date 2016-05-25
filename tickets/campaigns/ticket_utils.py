40a95956ee408d7bd798963b49060c3c544502d5
Roman Rader <roman.rader@gmail.com>
2016-05-25 20:41:48 +0300

    ticket title

--- tickets/campaigns/ticket_utils.py
+++ tickets/campaigns/ticket_utils.py
@@ -14,7 +14,7 @@ def notify_owner(ticket):
     logger.info("Sending the notification to {}".format(ticket.get_cart().email))
     headers = {'Reply-To': REPLY_EMAIL}
     body = render_to_string('campaigns/issuedticket_email.html', {'issuedticket': ticket})
-    msg = EmailMessage("Ваш білет на {}".format(ticket.get_cart().ticket_type.campaign.title), body,
+    msg = EmailMessage("Ваш квиток на {}".format(ticket.get_cart().ticket_type.campaign.title), body,
                        FROM_EMAIL, [ticket.get_cart().email], headers=headers)
     msg.content_subtype = "html"
     msg.send()
