79575674858fb927df36ed5725211fc6c18953c4
Roman Rader <roman.rader@gmail.com>
2016-05-26 20:13:14 +0300

    full site url in the email

--- tickets/campaigns/ticket_utils.py
+++ tickets/campaigns/ticket_utils.py
@@ -2,6 +2,7 @@ import logging
 from uuid import uuid4
 
 from campaigns.models import IssuedTicket
+from django.contrib.sites.models import Site
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from tickets.settings import REPLY_EMAIL, FROM_EMAIL
@@ -13,7 +14,8 @@ logger = logging.getLogger(__name__)
 def notify_owner(ticket):
     logger.info("Sending the notification to {}".format(ticket.get_cart().email))
     headers = {'Reply-To': REPLY_EMAIL}
-    body = render_to_string('campaigns/issuedticket_email.html', {'issuedticket': ticket})
+    body = render_to_string('campaigns/issuedticket_email.html', {'issuedticket': ticket,
+                                                                  'site': Site.objects.get_current()})
     msg = EmailMessage("Ваш квиток на {}".format(ticket.get_cart().ticket_type.campaign.title), body,
                        FROM_EMAIL, [ticket.get_cart().email], headers=headers)
     msg.content_subtype = "html"
