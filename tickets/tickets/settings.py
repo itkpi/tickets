ede36021ccfc49461b5a6fb39ab23e21bdaa48b7
Roman Rader <roman.rader@gmail.com>
2016-06-02 00:07:38 +0300

    ukrainian

--- tickets/tickets/settings.py
+++ tickets/tickets/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uk-ua'
 
 TIME_ZONE = 'Europe/Kiev'
 
