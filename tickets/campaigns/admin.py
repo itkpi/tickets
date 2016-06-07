3ddf70399f1aa47eea7d90ecd9a2e5b4faba83ad
Roman Rader <roman.rader@gmail.com>
2016-06-08 00:47:05 +0300

    add ticket info

--- tickets/campaigns/admin.py
+++ tickets/campaigns/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from campaigns.models import Campaign, Cart, TicketType, IssuedTicket, LiqPayData, TicketCounter
 
+
+class CartInline(admin.StackedInline):
+    model = Cart
+    extra = 0
+
+
+class IssuedTicketAdmin(admin.ModelAdmin):
+    model = IssuedTicket
+    inlines = [CartInline, ]
+
+
 admin.site.register(Campaign)
 admin.site.register(TicketType)
 admin.site.register(Cart)
-admin.site.register(IssuedTicket)
+admin.site.register(IssuedTicket, IssuedTicketAdmin)
 admin.site.register(LiqPayData)
 admin.site.register(TicketCounter)
