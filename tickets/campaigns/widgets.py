38dd852cd248c4899043e984704aaaefa0838d98
Roman Rader <roman.rader@gmail.com>
2016-06-10 17:55:00 +0300

    count

--- tickets/campaigns/widgets.py
+++ tickets/campaigns/widgets.py
@@ -3,7 +3,7 @@ import re
 from dashing.widgets import NumberWidget, ListWidget, KnobWidget
 from django.db.models import Count, Sum
 
-from campaigns.models import IssuedTicket, Campaign, TicketCounter
+from campaigns.models import IssuedTicket, Campaign, TicketCounter, TicketType
 
 
 def get_tickets_queryset():
@@ -55,8 +55,8 @@ class CoutersWidget(KnobWidget):
         }
 
     def get_amount(self):
-        return get_tickets_queryset().values('cart__ticket_type__counter__amount', 'cart__ticket_type__counter__id'). \
-            distinct().aggregate(Sum('cart__ticket_type__counter__amount'))['cart__ticket_type__counter__amount__sum']
+        return TicketType.objects.exclude(public=False).values('counter__amount', 'counter__id').distinct(). \
+            aggregate(Sum('counter__amount'))['counter__amount__sum']
 
     def get_detail(self):
         return "{} квитків у продажу".format(self.get_amount())
